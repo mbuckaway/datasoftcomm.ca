8946d15bd3312b21c08dcb545f1aaaa63c8c22bf
Mark Buckaway <mark@buckaway.ca>
2020-02-09 16:47:46 -0500

    Initial commit datasoft website

---
+++ assets/config.js
@@ -0,0 +1,45 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "info@datasoftcomm.ca",
+        address: "Waterloo, ON",
+        social: [
+            
+            
+            
+            
+            
+            {
+                icon: 'linkedin',
+                link: "https:\/\/www.linkedin.com\/in\/markbuckaway\/"
+            },
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "",
+            title: "",
+            subtitle: "",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "Contact Me",
+            buttonLink: "",
+            biography: "Located in Waterloo, as Sr. Software Developer, I have over 15 years of experience in various industries and I have coded in C\/C++, C#\/.NET, Java, Perl, and other languages but currently, I focus on C++. I am knowledgeable about Linux, HPUX, Solaris, AIX, Windows,Novell Netware, Mac OSX operating systems and recently added Apple iOS and Objective-C to my development knowledgebase. I know the SDLC and Agile software development methods. While I work with individuals professing 4 year degrees, I am completely self-taught and I like nothing better than learning something new that I have never seen before. \n\nSpecialties: C\/C++, Multiplatform development with Windows, Unix (Linux\/HPUX\/Solaris\/AIX), and MacOSX.",
+            portraitImage: ""
+        }
+    ]
+};
